08821d18fee72c2e6536ac69f689b611032c910d
GuillaumeMaurin <guillaume.maurin@edu.ece.fr>
2018-12-03 14:41:02 +0900

    new design for upload media readme refresh and _installation too

--- views/register.php
+++ views/register.php
@@ -1,21 +1,7 @@
-<?php
-// show potential errors / feedback (from registration object)
-if (isset($registration)) {
-    if ($registration->errors) {
-        foreach ($registration->errors as $error) {
-            echo $error;
-        }
-    }
-    if ($registration->messages) {
-        foreach ($registration->messages as $message) {
-            echo $message;
-        }
-    }
-}
-?>
+
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
-<div class="center-block m-5 w-50 bg-light">
+<div class="center-block mx-auto m-5 w-50 bg-light">
    <!-- register form -->
    <form method="post" class="m-5" action="register.php" name="registerform">
 
@@ -39,8 +25,32 @@ if (isset($registration)) {
        <input type="submit" class="btn btn-primary mt-2" name="register" value="Register" />
    </div>
    </form>
-
+   <?php
+   // show potential errors / feedback (from registration object)
+   if (isset($registration)) {
+       if ($registration->errors) {
+           foreach ($registration->errors as $error) {
+               echo $error;
+           }
+       }
+       if ($registration->messages) {
+           foreach ($registration->messages as $message) {
+               //echo $message;
+               echo "<div class=\"alert alert-success w-75 mx-auto \" role=\"alert\">
+               Your Account as been created successfully !
+                  </div>" ;
+               $delay = 2;
+               $url='index.php';
+               header("Refresh: $delay;url=$url");
+           }
+       }
+   }
+   ?>
 
    <!-- backlink -->
    <a href="index.php">Back to Login Page</a>
 </div>
+
+<!--script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script-->
